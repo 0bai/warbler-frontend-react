ca77b7ad35d9a3d754152418232bc52b4d21cd22
Obai Alnajjar <obai@outlook.com>
2019-03-21 15:50:36 +0300

    Added compose to wrap multiple store enhancers & fixed the proxy url

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import rootReducer from "reducers";
-import {applyMiddleware, createStore} from "redux";
+import rootReducer from "./reducers";
+import {applyMiddleware, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 
 export function configureStore() {
 	// store
 	return createStore(
 		rootReducer,
-		applyMiddleware(thunk),
-		window.devToolsExtension ? window.devToolsExtension() : f => f
+		compose(applyMiddleware(thunk),
+			window.devToolsExtension ? window.devToolsExtension() : f => f)
 	);
 }
